425c0bda4b2933130543df6b149a709ee12029fc
Stefan <Stefan@192.168.1.230>
2020-05-19 10:34:03 +0200

    person

--- verein-verwaltung/src/prg/vereinverwaltung/domain/Person.java
+++ verein-verwaltung/src/prg/vereinverwaltung/domain/Person.java
@@ -1,5 +1,5 @@
 package prg.vereinverwaltung.domain;
-
+import java.util.Date;
 /**
  * Diese Klasse bildet eine Person ab.
  * 
@@ -8,6 +8,45 @@ package prg.vereinverwaltung.domain;
  */
 public class Person {
 
-	// TODO ...
-
+	private String vorname;
+	private String name;
+	private Date geburtsdatum; 
+	
+	private Kontakt Kontakt;
+	private Adresse Adresse;
+	
+	
+	public Person(String vorname, String name, Date geburtsdatum, int plz, String strasse, String ort, String land, int telefon, String email){
+		this.vorname=vorname;
+		this.name=name;
+		this.geburtsdatum=geburtsdatum;
+		
+		this.Kontakt = new Kontakt(telefon, email);
+		this.Adresse = new Adresse(plz, strasse, ort,land);
+		
+	}
+	
+	public void setVorname(String vorname) {
+		this.vorname=vorname;
+	}
+	
+	public String getVorname() {
+		return vorname;
+	}
+	
+	public void setName(String name) {
+		this.name=name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setGeburtsdatum(Date geburtsdatum) {
+		this.geburtsdatum=geburtsdatum;
+	}
+	
+	public Date getGeburtsdatum() {
+		return geburtsdatum;
+	}
 }
