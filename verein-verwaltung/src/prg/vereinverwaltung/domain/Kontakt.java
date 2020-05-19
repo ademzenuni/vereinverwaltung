6c5756c8ea5937f08afdf75bb72637f6576cd648
ademzenuni <a.zenuni@3w-group.ch>
2020-05-19 10:09:58 +0200

    aaaa

--- verein-verwaltung/src/prg/vereinverwaltung/domain/Kontakt.java
+++ verein-verwaltung/src/prg/vereinverwaltung/domain/Kontakt.java
@@ -4,29 +4,24 @@ package prg.vereinverwaltung.domain;
  * Diese Klasse bildet die Kontaktdaten ab.
  * 
  * @author jsucur
- * @version 1.0aaa
+ * @version 1.0
  */
+
 public class Kontakt {
-	
 
-	private String name;
-	private String vorname;
-	private String telefon;
+	private int telefon;
 	private String email;
-
 	
-	public Kontakt(String telefon, String email, String name, String vorname) {
+	public Kontakt(int telefon, String email) {
 		this.telefon=telefon;
 		this.email=email;
-		this.name=name;
-		this.vorname=vorname;
 	}
 
-	public String getTelefon() {
+	public int getTelefon() {
 		return telefon;
 	}
 
-	public void setTelefon(String telefon) {
+	public void setTelefon(int telefon) {
 		this.telefon = telefon;
 	}
 
@@ -36,22 +31,6 @@ public class Kontakt {
 
 	public void setEmail(String email) {
 		this.email = email;
-	}
-
-	public String getVorname() {
-		return vorname;
-	}
-
-	public void setVorname(String vorname) {
-		this.vorname = vorname;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
 	}
 
 
